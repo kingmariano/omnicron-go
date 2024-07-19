55a465c24b87dd2c0b8c778ef4d5afa5d934aca2
kingmariano <charlesozochukwu2004@gmail.com>
2024-07-19 09:04:29 +0100

    added more examples for speech to text model

---
+++ _examples/stt/lowSTT.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/kingmariano/omnicron-go"
+)
+
+func main() {
+	apiKey := "HwikyHTrh4DMP9ZV1PzOn3K+C4Il7N/RKurD5AjyoIE="
+	client := omnicron.NewClient(apiKey, omnicron.WithBaseURL("https://omnicron-latest.onrender.com/"))
+    audioFile, err := os.Open("sample.mp3")
+	if err != nil {
+		fmt.Printf("Error opening image file: %v\n", err)
+		return
+	}
+	res, err := client.LowSTTGeneration(context.Background(), omnicron.LowSTTModelAndParams{
+		Model: omnicron.WhisperModel,
+		Parameters: omnicron.LowSTTParams{
+			Audio: audioFile,
+		},
+	})
+	if err != nil {
+		fmt.Printf("Error making Video Generation request: %v\n", err)
+		return
+	}
+
+	// Marshal the response to JSON
+	jsonData, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Printf("Error marshaling JSON: %v\n", err)
+		return
+	}
+
+	// Write JSON data to a file
+	file, err := os.Create("file.json")
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Printf("Error writing JSON to file: %v\n", err)
+		return
+	}
+
+	fmt.Println("JSON data written to file.json successfully")
+}
