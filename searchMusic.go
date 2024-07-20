7c296068e4d7242e42742925cd2a7897a9244453
kingmariano <charlesozochukwu2004@gmail.com>
2024-07-20 11:37:02 +0100

    added inline comments for better description

--- searchMusic.go
+++ searchMusic.go
@@ -7,6 +7,7 @@ type MusicSearchRequest struct {
 	Limit int    `json:"limit,omitempty"`
 	Proxy string `json:"proxy,omitempty"`
 }
+// this function takes a song as input and returns a list of matching songs. Use the DownloadMusic function to download the song.
 func (c *Client) MusicSearch(ctx context.Context, req *MusicSearchRequest)(*GabsContainer, error){
 	if req.Song == ""{
 		return nil, ErrSongNotProvided
