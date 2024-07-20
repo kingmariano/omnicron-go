091c13eab6533704e9b94950034ad932f1f1b5e8
kingmariano <charlesozochukwu2004@gmail.com>
2024-07-20 05:13:18 +0100

    style: added inline comments

--- convertToMp3.go
+++ convertToMp3.go
@@ -10,6 +10,7 @@ type ConvertToMP3Params struct {
 	URL       string   `form:"url,omitempty"`
 	File *os.File `form:"file,omitempty"`
 }
+// convert to mp3 function accepts the request parameters converts either the URL string or audio file to mp3 and then returns the result clouding direct url.
 func (c *Client) ConvertToMP3(ctx context.Context, req *ConvertToMP3Params) (*Responseparams, error){
 	if req.URL == "" && req.File == nil {
         return nil, ErrConvertToMP3NoURLOrFile
