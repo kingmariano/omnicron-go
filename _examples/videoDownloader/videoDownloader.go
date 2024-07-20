a61e30da3e87efd7b076f170a042ea2125d3cf5d
kingmariano <charlesozochukwu2004@gmail.com>
2024-07-20 04:57:00 +0100

    added examples for video downloader

---
+++ _examples/videoDownloader/videoDownloader.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/kingmariano/omnicron-go"
+)
+
+func main() {
+	apiKey := "YOUR_API_KEY"
+	client := omnicron.NewClient(apiKey, omnicron.WithBaseURL("https://omnicron-latest.onrender.com/"))
+	res, err := client.DownloadVideo(context.Background(), &omnicron.VideoDownloadParams{
+		URL: "https://www.youtube.com/watch?v=T2fqLam1iuk",
+	})
+	if err != nil {
+		fmt.Printf("Error downloading video url : %v\n", err)
+		return
+	}
+
+	// Marshal the response to JSON
+	jsonData, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Printf("Error marshaling JSON: %v\n", err)
+		return
+	}
+
+	// Write JSON data to a file
+	file, err := os.Create("file.json")
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Printf("Error writing JSON to file: %v\n", err)
+		return
+	}
+
+	fmt.Println("JSON data written to file.json successfully")
+}
